24797a26a01dc5f88f8e9140b455bb694da1b929
Sladriix-dev <sladriix@gmail.com>
2023-04-08 00:35:40 +0200

    Reorganization of files + [ADD] Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+export default function Header() {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid bg-faded padding-media">
+                <div className="container padding-media">
+                    <nav className="navbar navbar-toggle-md navbar-light">
+                        <button
+                            className="navbar-toggler mt-3"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            data-bs-parent="#navbarSupportContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="true"
+                            aria-label="Toggle Navigation"
+                        >
+                            <span className="fa fa-bars"></span>
+                        </button>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="navbarSupportedContent">
+                                <li className="nav-item nav-link">Home</li>
+                                <li className="nav-item nav-link">Create</li>
+                                <li className="nav-item nav-link">About</li>
+                            </ul>
+                            <div className="row g-3">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item nav-link">Login</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/pages/AddEditBlog.jsx
@@ -0,0 +1,7 @@
+export default function AddEditBlog() {
+    return (
+        <div>
+            AddEditBlog
+        </div>
+    )
+}
